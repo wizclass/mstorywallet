1564108a7c81ed1519ae597069b46886c1236b8b
SeongKon <canoa950327@gmail.com>
2023-02-01 18:26:57 +0900

    관리자페이지 조직도 메뉴 추가, 회원수정 페이지 수정작업

--- adm/admin.menu650.php
+++ adm/admin.menu650.php
@@ -8,10 +8,10 @@ $menu['menu650'] = array (
 	// array('650160', '상품권 통계', G5_ADMIN_URL.'/shop_admin/sale2.php', 'sst_order_stats'),
 
 
-    // array('650000', '조직도보기', ''.G5_ADMIN_URL.'/member_tree.php','allowance_sett'),
-    // array('650100', '조직도(트리) 보기', ''.G5_ADMIN_URL.'/member_tree.php', 'bbs_board'),
-    // array('650200', '조직도(박스) 보기', ''.G5_ADMIN_URL.'/member_org.php', 'bbs_board'),
-    // array('650200', '조직도2 보기', ''.G5_ADMIN_URL.'/member_random_org.php', 'bbs_board')
+    array('650000', '조직도보기', ''.G5_ADMIN_URL.'/member_tree.php','allowance_sett'),
+    array('650100', '조직도(트리) 보기', ''.G5_ADMIN_URL.'/member_tree.php', 'bbs_board'),
+    array('650200', '조직도(박스) 보기', ''.G5_ADMIN_URL.'/member_org.php', 'bbs_board'),
+    array('650200', '조직도2 보기', ''.G5_ADMIN_URL.'/member_random_org.php', 'bbs_board')
 	
 );
 }else{
@@ -21,10 +21,10 @@ $menu['menu650'] = array (
         // array('650150', '상품권 구매내역', ''.G5_ADMIN_URL.'/giftcard_orderlist.php','gift_card_history'),
 	    // array('650160', '상품권 통계', G5_ADMIN_URL.'/shop_admin/sale2.php', 'sst_order_stats'),
 
-        // array('650000', '조직도보기', ''.G5_ADMIN_URL.'/member_tree.php','allowance_sett'),
-        // array('650100', '조직도(트리) 보기', ''.G5_ADMIN_URL.'/member_tree.php', 'bbs_board'),
-        // array('650200', '조직도(박스) 보기', ''.G5_ADMIN_URL.'/member_org.php', 'bbs_board'),
-        // array('650200', '조직도2(제타플러스) 보기', ''.G5_ADMIN_URL.'/member_random_org.php', 'bbs_board')
+        array('650000', '조직도보기', ''.G5_ADMIN_URL.'/member_tree.php','allowance_sett'),
+        array('650100', '조직도(트리) 보기', ''.G5_ADMIN_URL.'/member_tree.php', 'bbs_board'),
+        array('650200', '조직도(박스) 보기', ''.G5_ADMIN_URL.'/member_org.php', 'bbs_board'),
+        array('650200', '조직도2(제타플러스) 보기', ''.G5_ADMIN_URL.'/member_random_org.php', 'bbs_board')
     );
 }
 ?>
