e76254a3c21b277961d8a44c07220704a3b00001
ayoung <ay9318@naver.com>
2023-02-01 18:58:50 +0900

    회원가입 페이지 테스트 후 주석제거

--- theme/mstory/register_result.php
+++ theme/mstory/register_result.php
@@ -3,12 +3,12 @@ include_once('./_common.php');
 
 include_once(G5_THEME_PATH.'/_include/head.php');
 
-// if (isset($_SESSION['ss_mb_reg']))
-// 	$mb = get_member($_SESSION['ss_mb_reg']);
+if (isset($_SESSION['ss_mb_reg']))
+	$mb = get_member($_SESSION['ss_mb_reg']);
 
-// // 회원정보가 없다면 초기 페이지로 이동
-// if (!$mb['mb_id'])
-// 	goto_url(G5_URL);
+// 회원정보가 없다면 초기 페이지로 이동
+if (!$mb['mb_id'])
+	goto_url(G5_URL);
 ?>
 <style>
     .container {
